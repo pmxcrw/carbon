997af95d4545017c604f51d8ffb608573c5ec6d6
Ceri Williams <ceri.rhys.williams@gmail.com>
2016-06-05 20:25:43 +0100

    actually adding files with LoadShapedDateRange

---
+++ core/time_period/load_shaped_date_range.py
@@ -0,0 +1,135 @@
+from core.time_period.date_range import DateRange, NEVER_DR
+from core.time_period.load_shape import LoadShape, BASE
+
+import datetime as dt
+
+
+class LoadShapedDateRange(object):
+
+    def __init__(self, date_range, load_shape=BASE):
+        if isinstance(date_range, str):
+            date_range = DateRange(date_range.lower().strip())
+        if isinstance(load_shape, str):
+            load_shape = LoadShape(load_shape.lower().strip())
+        if isinstance(date_range, DateRange) and\
+           isinstance(load_shape, LoadShape):
+            if date_range == NEVER_DR or load_shape == LoadShape(0):
+                self.date_range = NEVER_DR
+                self.load_shape = LoadShape(0)
+            else:
+                self.date_range = date_range
+                self.load_shape = load_shape
+        else:
+            msg = "inputs must be a DateRange object and LoadShape"
+            msg += " object, or strings that can be parsed into these"
+            raise ValueError(msg)
+
+    def __repr__(self):
+        return "LoadShapedDateRange(date_range={}, load_shape={})"\
+                .format(self.date_range, self.load_shape)
+
+    def __eq__(self, rhs):
+        if self.__class__ == rhs.__class__:
+            eq = self.date_range == rhs.date_range
+            eq &= self.load_shape == rhs.load_shape
+            return eq
+        else:
+            return False
+
+    def __hash__(self):
+        return hash((self.date_range, self.load_shape))
+
+    @property
+    def start(self):
+        return self.date_range.start
+
+    @property
+    def end(self):
+        return self.date_range.end
+
+    @property
+    def duration(self):
+        '''returns the duration in days'''
+        weekdays, weekends = self.date_range.weekday_and_weekend_duration
+        duration = weekdays * self.load_shape.weekday_load_factor
+        duration += weekends * self.load_shape.weekend_load_factor
+        return duration
+
+    def intersection(self, other):
+        if self.load_shape.intersects(other.load_shape):
+            if self.date_range.intersects(other.date_range):
+                date_range = self.date_range.intersection(other.date_range)
+                load_shape = self.load_shape.intersection(other.load_shape)
+                return LoadShapedDateRange(date_range, load_shape)
+        return NEVER_LSDR
+
+    def intersects(self, other):
+        if self.load_shape.intersects(other.load_shape):
+            if self.date_range.intersects(other.date_range):
+                # this is expensive to compute, so first test the basic
+                # intersections to see if we nest inside the cheaper
+                # calculations and return false sooner if we can
+                return self.intersection(other).duration > 0
+        return False
+
+    def equivalent(self, RHS):
+        '''tests whether two LoadShapedDateRange objects represent the same
+        set of hours.
+
+        Computationally expensive'''
+        intersection = self.intersection(RHS)
+        duration = self.duration
+        if duration != intersection.duration:
+            return False
+        else:
+            return duration == RHS.duration
+
+    def __contains__(self, LHS):
+        if isinstance(LHS, (dt.date, DateRange)):
+            if LHS in self.date_range:
+                return self.load_shape == BASE
+        elif LHS.date_range in self.date_range:
+                return LHS.load_shape in self.load_shape
+        else:
+            return False
+
+    def split_by_range_type(self, range_type):
+        date_ranges = self.date_range.split_by_range_type(range_type)
+        return [LoadShapedDateRange(dr, self.load_shape) for dr in date_ranges]
+
+    @property
+    def split_by_month(self):
+        date_ranges = self.date_range.split_by_month
+        return [LoadShapedDateRange(dr, self.load_shape) for dr in date_ranges]
+
+    @property
+    def split_by_quarter(self):
+        date_ranges = self.date_range.split_by_quarter
+        return [LoadShapedDateRange(dr, self.load_shape) for dr in date_ranges]
+
+    def __iter__(self):
+        week_hours = self.load_shape.weekday_load_factor
+        weekend_hours = self.load_shape.weekend_load_factor
+        for d in self.date_range:
+            weekday = d.weekday()
+            if weekday < 5 and week_hours:
+                yield LoadShapedDateRange(DateRange(d, d), self.load_shape)
+            elif weekday > 4 and weekend_hours:
+                yield LoadShapedDateRange(DateRange(d, d), self.load_shape)
+
+    def difference(self, other):
+        '''
+        Returns a tripple of DateRange objects:
+
+        1) The days in self which are before other (with self's loadshape)
+        2) The days in both self and other, with load shape of self - other
+        3) The days in self which are after other (with self's loadshape)
+        '''
+        start, end = self.date_range.difference(other.date_range)
+        mid = self.date_range.intersection(other.date_range)
+        load_shape = self.load_shape.difference(other.load_shape)
+        return [LoadShapedDateRange(start, self.load_shape),
+                LoadShapedDateRange(mid, load_shape),
+                LoadShapedDateRange(end, self.load_shape)]
+
+NEVER_LSDR = LoadShapedDateRange(NEVER_DR, LoadShape(0))
---
+++ core/time_period/tests/test_load_shaped_date_range.py
@@ -0,0 +1,184 @@
+from core.time_period.date_range import DateRange, YearType, DayType
+from core.time_period.load_shaped_date_range import LoadShapedDateRange,\
+                                                    NEVER_LSDR
+from core.time_period.load_shape import BASE, PEAK, OFFPEAK,\
+                                        WEEKDAY_OFFPEAK,\
+                                        WEEKEND_OFFPEAK,\
+                                        WEEKEND_PEAK,\
+                                        EFAS,\
+                                        WEEKEND,\
+                                        NIGHTTIME,\
+                                        HOURS,\
+                                        WEEKDAY_HOURS,\
+                                        NEVER_LS
+
+import datetime as dt
+import unittest
+
+
+class LoadShapedDateRangeTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.dec = DateRange('2012-M12')
+        self.dec_base = LoadShapedDateRange('2012-M12', 'Base')
+        self.dec_peak = LoadShapedDateRange('2012-M12', 'Peak')
+        self.dec_offpeak = LoadShapedDateRange(self.dec, OFFPEAK)
+        self.q4 = DateRange('2012-Q4')
+        self.sum12 = DateRange('2012-SUM')
+        self.sat = DateRange(dt.date(2012, 12, 8), dt.date(2012, 12, 8))
+        self.sun = self.sat.offset(1)
+        self.sat_base = LoadShapedDateRange(self.sat)
+        self.sat_offpeak = LoadShapedDateRange(self.sat, 'Offpeak')
+        self.sat_peak = LoadShapedDateRange(self.sat, PEAK)
+        self.sun_peak = LoadShapedDateRange(self.sun, 'Peak')
+        self.fri = self.sat.offset(-1)
+        self.fri_base = LoadShapedDateRange(self.fri, BASE)
+        self.fri_peak = LoadShapedDateRange(self.fri, PEAK)
+        self.fri_offpeak = LoadShapedDateRange(self.fri, OFFPEAK)
+        self.jan_efa1 = LoadShapedDateRange('2014-M1', 'EFA1')
+
+    def test_init(self):
+        self.assertEqual(self.dec_base.date_range, self.dec)
+        self.assertEqual(self.dec_base.load_shape, BASE)
+        self.assertEqual(self.sat_base.date_range, self.sat)
+        self.assertEqual(self.sat_base.load_shape, BASE)
+
+    def test_null(self):
+        null = LoadShapedDateRange(self.sat, PEAK)
+        self.assertEqual(null.duration, 0)
+
+    def test_duration(self):
+        self.assertEqual(self.dec_peak.duration, 10.5)
+        self.assertEqual(self.dec_offpeak.duration, 20.5)
+        self.assertAlmostEqual(self.jan_efa1.duration, 31 * 4 / 24)
+
+        # dec_12 has 10 weekend-days and 21 weekdays
+        expected = {BASE: 31,
+                    PEAK: 21 * 0.5,
+                    OFFPEAK: 21 * 0.5 + 10,
+                    WEEKDAY_OFFPEAK: 21 * 0.5,
+                    WEEKEND_OFFPEAK: 10 * 0.5,
+                    WEEKEND_PEAK: 10 * 0.5,
+                    WEEKEND: 10,
+                    NIGHTTIME: 31 * 0.5,
+                    NEVER_LS: 0,
+                    HOURS[2]: 31 / 24,
+                    WEEKDAY_HOURS[2]: 21 / 24}
+        for load_shape, expected in expected.items():
+            lsdr = LoadShapedDateRange('2012-M12', load_shape)
+            self.assertAlmostEqual(lsdr.duration, expected)
+
+    def test_eq(self):
+        self.assertNotEqual(self.sat, self.sat_offpeak)
+        self.assertNotEqual(self.sat_offpeak, self.sat)
+        self.assertNotEqual(self.sat, self.sat_base)
+        self.assertNotEqual(self.sat_offpeak, self.sat_base)
+        self.assertNotEqual(self.sat_peak,  NEVER_LSDR)
+
+    def test_equivalent(self):
+        # equivalance means the same delivery hours
+        self.assertTrue(self.sat_base.equivalent(self.sat_offpeak))
+        self.assertFalse(self.fri_base.equivalent(self.fri_offpeak))
+        self.assertTrue(self.sat_peak.equivalent(NEVER_LSDR))
+        self.assertTrue(self.sat_peak.equivalent(self.sun_peak))
+
+    def test_intersection(self):
+        self.assertEqual(self.fri_peak.intersection(self.fri_offpeak),
+                         NEVER_LSDR)
+        self.assertEqual(self.sat_peak.intersection(self.fri_peak),
+                         NEVER_LSDR)
+        self.assertEqual(self.dec_offpeak.intersection(self.fri_base),
+                         self.fri_offpeak)
+
+    def test_intersects(self):
+        self.assertFalse(self.sat_base.intersects(self.sun_peak))
+        self.assertTrue(self.fri_base.intersects(self.fri_peak))
+        self.assertTrue(self.fri_peak.intersects(self.fri_base))
+        self.assertFalse(self.fri_peak.intersects(self.fri_offpeak))
+        self.assertFalse(self.sat_peak.intersects(self.sat_base))
+        # intersection is sat_peak, which has a duration of zero
+        # intersects method only returns true if duration is non-zero
+
+    def test_start(self):
+        self.assertEqual(self.dec_peak.start, dt.date(2012, 12, 1))
+
+    def test_end(self):
+        self.assertEqual(self.dec_peak.end, dt.date(2012, 12, 31))
+
+    def test_contains(self):
+        self.assertTrue(LoadShapedDateRange('2012-Q4', PEAK) in
+                        LoadShapedDateRange('2012-Q4', BASE))
+        self.assertFalse(LoadShapedDateRange('2012-Q4', BASE) in
+                         LoadShapedDateRange('2012-Q4', PEAK))
+        self.assertTrue(LoadShapedDateRange('2012-M11', BASE) in
+                        LoadShapedDateRange('2012-Q4', BASE))
+        self.assertFalse(LoadShapedDateRange('2012-Q4', BASE) in
+                         LoadShapedDateRange('2012-M11', BASE))
+        self.assertTrue(LoadShapedDateRange('2012-M11', HOURS[1]) in
+                        LoadShapedDateRange('2012-Q4', EFAS[0]))
+        self.assertFalse(LoadShapedDateRange('2012-Q4', EFAS[0]) in
+                         LoadShapedDateRange('2012-M11', HOURS[1]))
+
+    def test_split_by_month(self):
+        lsdr = LoadShapedDateRange(DateRange(dt.date(2011, 11, 17),
+                                             dt.date(2012, 3, 4)),
+                                   PEAK)
+        ranges = [DateRange(dt.date(2011, 11, 17), dt.date(2011, 11, 30)),
+                  DateRange('2011-M12'),
+                  DateRange('2012-M1'),
+                  DateRange('2012-M2'),
+                  DateRange(dt.date(2012, 3, 1), dt.date(2012, 3, 4))]
+        self.assertEqual(lsdr.split_by_month,
+                         [LoadShapedDateRange(x, PEAK) for x in ranges])
+
+    def test_split_by_quarter(self):
+        lsdr = LoadShapedDateRange(DateRange(dt.date(2011, 11, 17),
+                                             dt.date(2012, 3, 4)),
+                                   PEAK)
+        ranges = [DateRange(dt.date(2011, 11, 17), dt.date(2011, 12, 31)),
+                  DateRange(dt.date(2012, 1, 1), dt.date(2012, 3, 4))]
+        self.assertEqual(lsdr.split_by_quarter,
+                         [LoadShapedDateRange(x, PEAK) for x in ranges])
+
+    def test_split_by_range_type(self):
+        lsdr = LoadShapedDateRange(DateRange(dt.date(2011, 11, 17),
+                                             dt.date(2013, 3, 4)),
+                                   PEAK)
+        ranges = [DateRange(dt.date(2011, 11, 17), dt.date(2011, 12, 31)),
+                  DateRange('2012'),
+                  DateRange(dt.date(2013, 1, 1), dt.date(2013, 3, 4))]
+        self.assertEqual(lsdr.split_by_range_type(YearType),
+                         [LoadShapedDateRange(x, PEAK) for x in ranges])
+
+    def test_iter(self):
+        ls = LoadShapedDateRange(DateRange(dt.date(2011, 11, 17),
+                                           dt.date(2011, 11, 23)),
+                                 PEAK)
+        dr = [DateRange(dt.date(2011, 11, 17), range_type='d')]
+        dr += [DateRange(dt.date(2011, 11, 18), dt.date(2011, 11, 18))]
+        drl = DateRange(dt.date(2011, 11, 21), dt.date(2011, 11, 23))
+        dr += drl.split_by_range_type(DayType)
+        self.assertEqual([l for l in ls],
+                         [LoadShapedDateRange(i, PEAK) for i in dr])
+
+    def test_difference(self):
+        q4_base = LoadShapedDateRange(self.q4, BASE)
+        win12_base = LoadShapedDateRange('2012-WIN', 'Base')
+        q1_13 = win12_base.difference(q4_base)
+        self.assertTrue(q1_13[0].equivalent(NEVER_LSDR) and
+                        q1_13[1].equivalent(NEVER_LSDR) and
+                        q1_13[2].equivalent(LoadShapedDateRange('2013-Q1',
+                                                                'Base')))
+        never = q4_base.difference(win12_base)
+        self.assertTrue(never[0].equivalent(NEVER_LSDR) and
+                        never[1].equivalent(NEVER_LSDR) and
+                        never[2].equivalent(NEVER_LSDR))
+
+        winter = LoadShapedDateRange('2012')
+        winter = winter.difference(LoadShapedDateRange('2012-SUM', 'Peak'))
+        self.assertTrue(winter[0].equivalent(LoadShapedDateRange('2012-Q1'))
+                        and
+                        winter[1].equivalent(LoadShapedDateRange('2012-SUM',
+                                                                 'Offpeak'))
+                        and
+                        winter[2].equivalent(LoadShapedDateRange('2012-Q4')))
