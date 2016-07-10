ebed180e6e138dbae86b824371cfd353c5a91bb9
Ceri Williams <ceri.rhys.williams@gmail.com>
2016-07-10 19:42:18 +0100

    completed commodity_forward_curve; unit tests still to do.

--- core/forward_curves/commodity_forward_curve.py
+++ core/forward_curves/commodity_forward_curve.py
@@ -19,12 +19,13 @@ class CommodityForwardCurve(AbstractForwardCurve):
         self._discount_curve = discount_curve
         self._shape_algorithm = shape_algorithm
         self._settlement_rule = quotes.settlement_rule
+        self._time_period_parition_set = quotes.time_period_set.parition
         self._prices = self._generate_disjoint_prices(quotes)
         self._calc_price_cache = {}
 
     def _generate_disjoint_prices(self, quotes):
         quoted_time_periods = sorted(quotes.time_period_set, key=str)
-        disjoint_time_periods = sorted(quotes.time_period_set.partition, key=str)
+        disjoint_time_periods = sorted(self.time_period_set, key=str)
         quoted_values = np.array([quotes[time_period] for time_period in quoted_time_periods])
         matrix = self._transform_time_periods(quoted_time_periods, disjoint_time_periods)
         disjoint_prices = np.linalg.solve(matrix, quoted_values)
@@ -49,10 +50,37 @@ class CommodityForwardCurve(AbstractForwardCurve):
             matrix.append(matrix_row)
         return np.array(matrix)
 
-    def price(self, time_period, intraday_shape_algorithm=None):
+    def price(self, time_period):
         if time_period not in self._calc_price_cache:
-            self._calc_price_cache[time_period] = self._calculate_price(time_period, intraday_shape_algorithm)
+            self._calc_price_cache[time_period] = self._calculate_price(time_period)
         return self._calc_price_cache[time_period] * self.unit
 
-    def _calculate_price(self, required_time_period, intraday_shape_algorithm):
-        known_time_periods = self.
+    def _calculate_price(self, required_time_period):
+        known_time_period_sets = set(partition for partition in self._time_period_parition_set
+                                 if partition.intersects(required_time_period))
+        total_price = 0
+        total_time = 0 * DAY
+        for known_time_period_set in known_time_period_sets:
+            intersecting_time_period_set = known_time_period_set.intersection(required_time_period)
+            time = self._discounted_duration_of_time_set(intersecting_time_period_set)
+            if time > 0:
+                unshaped_price = self._prices[known_time_period_set]
+                shape_ratio = self._shape_algorithm.time_period_set_ratio(intersecting_time_period_set,
+                                                                          known_time_period_set)
+                price = unshaped_price * shape_ratio
+                total_price += price * time
+                total_time += time
+
+        # check whether the known time periods (for which we have prices) cover the required time period
+        # allow a SECOND of difference given that we're dealing with floats
+        required_duration = required_time_period.discounted_duration(self._settlement_rule, self._discount_curve)
+        difference = abs(total_time - required_duration)
+        if difference > 1 / SECONDS_PER_DAY * DAY:
+            msg = "can't calulate price for time period: {}".format(required_time_period)
+            msg += " the forward curve doesn't span sufficient range (out by {})".format(difference)
+            raise MissingPriceError(msg)
+
+        try:
+            return total_price / total_time
+        except Exception:
+            raise MissingPriceError("Couldn't calculate price (null delivery?): {}".format(str(required_time_period)))
